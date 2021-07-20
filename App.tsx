af80bc70304b9d9baea6844056b2540b83aac45d
soujvnunes <soujvnunes@gmail.com>
2021-07-20 00:10:26 -0300

    Using screens at App

--- App.tsx
+++ App.tsx
@@ -3,25 +3,24 @@ import { StatusBar } from "expo-status-bar";
 import React from "react";
 import { NavigationContainer } from "@react-navigation/native";
 import { createStackNavigator } from "@react-navigation/stack";
-import { Home } from "./screens";
+import { Breed, Home } from "./screens";
 import { SafeAreaProvider } from "react-native-safe-area-context";
-import { Header } from "./components";
+import { Header } from "./containers";
 
 const Stack = createStackNavigator();
 
 export default function App(): JSX.Element {
+  const options = {
+    header: (props) => <Header {...props} />,
+  };
+
   return (
     <SafeAreaProvider>
       <NavigationContainer>
         <StatusBar style="light" />
         <Stack.Navigator>
-          <Stack.Screen
-            name="Home"
-            component={Home}
-            options={{
-              header: (props) => <Header {...props} />,
-            }}
-          />
+          <Stack.Screen name="Home" component={Home} options={options} />
+          <Stack.Screen name="Breed" component={Breed} options={options} />
         </Stack.Navigator>
       </NavigationContainer>
     </SafeAreaProvider>
