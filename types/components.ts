f49d592de296ddda3a277bc74fbef648b9f54d81
soujvnunes <soujvnunes@gmail.com>
2021-07-20 11:29:04 -0300

    Adding missing types

--- types/components.ts
+++ types/components.ts
@@ -1,9 +1,14 @@
-import { ViewProps } from "react-native";
+import { ReactNode } from "react";
+import { TextProps, TouchableOpacityProps, ViewProps } from "react-native";
 import { SafeAreaViewProps } from "react-native-safe-area-context";
 import { SvgProps } from "react-native-svg";
 
+export interface Button extends TouchableOpacityProps {
+  icon: Icon["children"];
+  children: ReactNode;
+}
 export interface Icon extends SvgProps {
-  children: "logo" | "filter" | "weight" | "origin" | string;
+  children: "logo" | "filter" | "weight" | "origin" | "extern" | string;
   color?: "main" | "accent" | "textPrimary" | "textSecondary" | "textTertiary";
   size?: "smaller" | "small" | "medium" | "large" | "larger";
 }
@@ -11,3 +16,7 @@ export interface Header extends ViewProps {}
 export interface GridItem extends ViewProps {}
 export interface Grid extends ViewProps {}
 export interface SafeAreaView extends SafeAreaViewProps {}
+export interface Text extends TextProps {
+  variant?: "text" | "label";
+  color?: Icon["color"];
+}
