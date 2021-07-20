11596f4a2632b5f65504d195c9e5075496e69cd0
soujvnunes <soujvnunes@gmail.com>
2021-07-20 00:09:40 -0300

    Removing unused files

--- types.ts
+++
@@ -1,9 +0,0 @@
-import { ViewProps } from "react-native";
-import { SvgProps } from "react-native-svg";
-
-export interface Icon extends SvgProps {
-  children: "logo" | "filter" | string;
-  color?: "main" | "accent" | "textPrimary" | "textSecondary" | "textTertiary";
-  size?: "smaller" | "small" | "medium" | "large" | "larger";
-}
-export interface Header extends ViewProps {}
